53fdb6eaf5a4a56c361769cee604af42179aff59
neiderm <ded.reckning@gmail.com>
2026-07-23 11:49:03 -0400

    dos2unix

--- tools/galaga_unzip/make_galaga_proms.sh
+++ tools/galaga_unzip/make_galaga_proms.sh
@@ -3,7 +3,7 @@ cat 3200a.bin 3300b.bin 3400c.bin 3500d.bin > galaga_cpu1.bin
 ./make_vhdl_prom  3600e.bin       galaga_cpu2.vhd
 ./make_vhdl_prom  3700g.bin       galaga_cpu3.vhd
 
-##### difficult to find 2600j.bin 2800l.bin 2700k.bin 
+##### difficult to find 2600j.bin 2800l.bin 2700k.bin
 ./make_vhdl_prom  2600j.bin       bg_graphx.vhd
 cat 2800l.bin 2700k.bin > sp_graphx.bin
 ./make_vhdl_prom  sp_graphx.bin   sp_graphx.vhd
